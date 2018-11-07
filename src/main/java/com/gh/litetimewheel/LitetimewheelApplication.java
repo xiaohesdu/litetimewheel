ac811d431ebe5db81882684dcd22fa5eee53fcc4
gonghe.hogan <gonghe123456>
2018-11-07 14:21:30 +0800

    first commit

---
+++ src/main/java/com/gh/litetimewheel/LitetimewheelApplication.java
@@ -0,0 +1,12 @@
+package com.gh.litetimewheel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LitetimewheelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LitetimewheelApplication.class, args);
+	}
+}
